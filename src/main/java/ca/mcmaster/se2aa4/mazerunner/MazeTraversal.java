61470c3d0c24ac4ab0036be20336be49c199a406
eric-solak <solakeric28@gmail.com>
2024-01-26 12:30:08 -0500

    fixed issue with interface

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeTraversal.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeTraversal.java
@@ -2,7 +2,7 @@ package ca.mcmaster.se2aa4.mazerunner;
 
 import java.io.IOException;
 
-public interface MazeTraversal {
+public interface  MazeTraversal {
     String traverseMaze() throws IOException;
     String testPath(String path) throws IOException;
 
